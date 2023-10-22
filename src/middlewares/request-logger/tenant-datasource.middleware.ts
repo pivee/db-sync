e2e9e834ae72a335df9bb3738437722fef079ec2
Pivithuru Jayakody <pivithurujayakody@gmail.com>
2023-10-22 09:09:58 +0530

    Update TenantSourceMiddleware

--- src/middlewares/request-logger/tenant-datasource.middleware.ts
+++ src/middlewares/request-logger/tenant-datasource.middleware.ts
@@ -1,13 +1,26 @@
+import { MainPrismaService } from '@/modules/prisma/main-prisma.service';
 import { IRequestWithProps } from '@/types/IRequestWithProps';
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { BadRequestException, Injectable, NestMiddleware, NotFoundException } from '@nestjs/common';
 import { Response } from 'express';
 
 @Injectable()
 export class TenantSourceMiddleware implements NestMiddleware {
-  use(request: IRequestWithProps, response: Response, next: () => void) {
+  constructor(private readonly mainPrisma: MainPrismaService) {}
+
+  async use(request: IRequestWithProps, response: Response, next: () => void) {
+    const tenantCode = request.headers["x-tenant-code"] as string;
+
+    const tenant = await this.mainPrisma.tenant.findFirst({
+      include: { datasource: true },
+      where: { code: tenantCode }
+    });
+
+    if (!tenant) throw new BadRequestException("Invalid tenant code.");
+    if (!tenant.datasource) throw new NotFoundException("This tenant has no datasource.");
+
     request.tenant = {
-      tenantCode: request.headers["x-tenant-code"] as string,
-      datasourceUrl: process.env.TENANT_DATABASE_URL
+      tenantCode: tenantCode,
+      datasourceUrl: tenant.datasource.url
     };
 
     next();
